e09090c9a6240d4438b32a52faa132b6ca6b4177
Natalie Clark <nj_clark@hotmail.co.uk>
2023-12-14 11:56:35 +0000

    Add all background colours

--- app/src/main/java/com/example/lingle/ui/theme/Color.kt
+++ app/src/main/java/com/example/lingle/ui/theme/Color.kt
@@ -9,3 +9,23 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Homepage
+val lightBlue = Color(0xFFB5FFFB)
+val darkBlue = Color(0xFF07A69E)
+
+// Fruit Category
+val lightOrange = Color(0xFFFFF0C5)
+val darkOrange = Color(0xFFFFBF00)
+
+// Colours Category
+val lightPurple =Color(0xFFE2C2E8)
+val darkPurple =Color(0xFFC08EC9)
+
+// Vegetables Category
+val lightGreen =Color(0xFFC6FFE4)
+val darkGreen =Color(0xFF00D572)
+
+// Animals Category
+val lightRed =Color(0xFFFFB2B2)
+val darkRed =Color(0xFFFF5959)
