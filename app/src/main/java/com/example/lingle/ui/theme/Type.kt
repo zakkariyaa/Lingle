6d303318369c00d2d354df122a38c4a23c55f9f0
Natalie Clark <nj_clark@hotmail.co.uk>
2023-12-15 10:52:58 +0000

    Add fontweight and sp imports to fix bug

--- app/src/main/java/com/example/lingle/ui/theme/Type.kt
+++ app/src/main/java/com/example/lingle/ui/theme/Type.kt
@@ -4,6 +4,8 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.lingle.R
 
 val fonts = FontFamily(
